9306a4e77d348fff949c493803a653e3b4f4140f
bolshakov.mp <email@bk.ru>
2026-04-01 00:37:26 +0300

    release chunks sort

--- task1/main.c
+++ task1/main.c
@@ -1,14 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include "array/array.h"
-
-
-int array_read(Array *array);
-int process(Array *array);
-
-
-const int INITIAL_CAPACITY = 16;
+#include "main.h"
 
 
 int main(int argc, char* argv[])
@@ -37,7 +27,7 @@ int main(int argc, char* argv[])
     if (checker != 0)
         return checker;
 
-    process(&array);
+    process(&array, threads_count);
 
     array_output(&array);
 
@@ -56,7 +46,59 @@ int array_read(Array *array)
 }
 
 
-int process(Array *array)
+int process(Array *array, unsigned int threads_count)
 {
+    sort_parts(array, threads_count);
+}
 
+
+int sort_parts(Array *array, unsigned int threads_count)
+{
+    if (threads_count > array->size)
+        threads_count = array->size;
+
+    pthread_t *threads = malloc(sizeof(pthread_t) * threads_count);
+    ThreadData *tasks = malloc(sizeof(ThreadData) * threads_count);
+    int *buffer = malloc(sizeof(int) * array->size);
+    int *lefts = malloc(sizeof(int) * threads_count);
+    int *rights = malloc(sizeof(int) * threads_count);
+    int result = 1;
+
+    if (threads == NULL || tasks == NULL || buffer == NULL ||
+            lefts == NULL || rights == NULL)
+        goto cleanup;
+
+    int base = array->size / threads_count;
+    int rem = array->size % threads_count;
+    int start = 0;
+    for (unsigned int thread_index = 0; thread_index < threads_count; thread_index++)
+    {
+        int part_size = base;
+        if (thread_index < rem)
+            part_size++;
+
+        tasks[thread_index].data = array->data;
+        tasks[thread_index].left = start;
+        tasks[thread_index].right = start + part_size;
+
+        lefts[thread_index] = tasks[thread_index].left;
+        rights[thread_index] = tasks[thread_index].right;
+
+        start = tasks[thread_index].right;
+
+        if (pthread_create(&threads[thread_index], NULL, array_sort, &tasks[thread_index]) != 0)
+            goto cleanup;
+    }
+
+    for (unsigned int thread_index = 0; thread_index < threads_count; thread_index++)
+        if (pthread_join(threads[thread_index], NULL) != 0)
+            goto cleanup;
+
+cleanup:
+    free(threads);
+    free(tasks);
+    free(buffer);
+    free(lefts);
+    free(rights);
+    return result;
 }
---
+++ task1/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "array/array.h"
+#include "sort/sort.h"
+
+
+typedef struct {
+    int *data;
+    int left;
+    int right;
+} ThreadData;
+
+
+int array_read(Array *array);
+int process(Array *array, unsigned int threads_count);
+
+
+const int INITIAL_CAPACITY = 16;
+
+
+#endif
